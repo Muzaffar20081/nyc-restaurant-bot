0f674915115ee623295ab0538491293c0fabb086
Muzaffar <160494337+Muzaffar20081@users.noreply.github.com>
2025-12-05 20:31:47 -0500

    Reorganize config.py structure and order

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 BOT_TOKEN = "8244967100:AAFG7beMN450dqwzlqQDjnFJoHxWl0qjXAE"
-ADMIN_IDS = [6056106251]  # Ваш Telegram ID
 
 CAFES = {
     "italy": {
@@ -17,4 +16,6 @@ CAFES = {
 }
 
 DEFAULT_CAFE = "italy"
+ADMIN_IDS = [6056106251]  # Ваш Telegram ID
+
 
