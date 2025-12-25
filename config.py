b3457500792d29c34d970b358ca048fe1ed7b24d
Muzaffar <160494337+Muzaffar20081@users.noreply.github.com>
2025-12-25 17:20:41 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,6 @@
 BOT_TOKEN = "8244967100:AAFG7beMN450dqwzlqQDjnFJoHxWl0qjXAE"
-ADMIN_IDS = [6056106251]  # Ваш ID Telegram
-
-CAFES = {
-    "italy": {
-        "name": "🍝 Итальянское кафе 'Рома'",
-        "description": "Настоящая итальянская кухня"
-    },
-    "sushi": {
-        "name": "🍣 Суши-бар 'Токио'",
-        "description": "Свежие суши и роллы"
-    },
-    "burger": {
-        "name": "🍔 Бургер-хаус 'Техас'",
-        "description": "Сочные бургеры"
-    }
+CUISINES = {
+    "burgers": "🍔 Бургеры NYC",
+    "italy": "🍝 Итальянская",
+    "sushi": "🍣 Суши"
 }
-
-DEFAULT_CAFE = "italy"
