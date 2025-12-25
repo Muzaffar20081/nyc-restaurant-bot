7697eac6c28f398a8fde44f7ba8d0bcbdb6a4a2b
Muzaffar <160494337+Muzaffar20081@users.noreply.github.com>
2025-12-25 17:33:04 -0500

    Update __init__.py

--- menus/__init__.py
+++ menus/__init__.py
@@ -1,9 +1,22 @@
-from .burger_menu import BURGER_MENU
-from .italy_menu import ITALY_MENU
-from .sushi_menu import SUSHI_MENU
+# menus/__init__.py
+from .burger_menu import ALL_ITEMS as BURGER_ITEMS, MENU_TEXT as BURGER_TEXT
+from .italy_menu import ALL_ITEMS as ITALY_ITEMS, MENU_TEXT as ITALY_TEXT
+from .sushi_menu import ALL_ITEMS as SUSHI_ITEMS, MENU_TEXT as SUSHI_TEXT
 
 MENU_BY_CUISINE = {
-    "burgers": BURGER_MENU,
-    "italy": ITALY_MENU,
-    "sushi": SUSHI_MENU
+    "burgers": {
+        "name": "🍔 Бургер-хоус",
+        "items": BURGER_ITEMS,
+        "text": BURGER_TEXT
+    },
+    "italy": {
+        "name": "🍕 Итальянская кухня",
+        "items": ITALY_ITEMS,
+        "text": ITALY_TEXT
+    },
+    "sushi": {
+        "name": "🍣 Суши-бар Токио",
+        "items": SUSHI_ITEMS,
+        "text": SUSHI_TEXT
+    }
 }
