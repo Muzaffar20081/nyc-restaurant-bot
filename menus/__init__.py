d0e1d8569af6a278be31ab33ded0a9ab842f30b8
Muzaffar <160494337+Muzaffar20081@users.noreply.github.com>
2025-12-29 10:24:59 -0500

    Update __init__.py

--- menus/__init__.py
+++ menus/__init__.py
@@ -3,7 +3,7 @@ from .burger_menu import BURGER_MENU
 from .italy_menu import ITALY_MENU
 from .sushi_menu import SUSHI_MENU
 
-# Объединяем все меню в один словарь
+# Объединяем все меню
 ALL_MENUS = {
     "burgers": BURGER_MENU,
     "pizza": ITALY_MENU,
@@ -21,14 +21,22 @@ def get_menu_by_category(category_id):
     """Получить меню по категории"""
     return ALL_MENUS.get(category_id, [])
 
-def get_all_categories():
-    """Получить все категории меню"""
-    return MENU_CATEGORIES
-
 def find_item_by_id(item_id):
     """Найти товар по ID во всех меню"""
-    for category in ALL_MENUS.values():
-        for item in category:
+    for category_items in ALL_MENUS.values():
+        for item in category_items:
             if item.get("id") == item_id:
                 return item
     return None
+
+def search_items(query):
+    """Поиск товаров по названию"""
+    results = []
+    query_lower = query.lower()
+    
+    for category_items in ALL_MENUS.values():
+        for item in category_items:
+            if query_lower in item["name"].lower():
+                results.append(item)
+    
+    return results
