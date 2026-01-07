eaa81810a725b604722747d1303db7e662790eb8
Muzaffar <160494337+Muzaffar20081@users.noreply.github.com>
2026-01-07 09:57:43 -0500

    Refactor menu initialization and exports

--- menus/__init__.py
+++ menus/__init__.py
@@ -1,20 +1,18 @@
-"""
-Инициализация модуля меню
-"""
+# menus/__init__.py
 from .burger_menu import BurgerMenu
 from .italy_menu import ItalyMenu
 from .sushi_menu import SushiMenu
 
-__all__ = ['BurgerMenu', 'ItalyMenu', 'SushiMenu']
-
-# Создаем глобальные экземпляры меню для использования во всем приложении
+# Создаем экземпляры меню
 burger_menu = BurgerMenu()
 italy_menu = ItalyMenu()
 sushi_menu = SushiMenu()
 
-# Словарь всех меню для быстрого доступа
+# Словарь для быстрого доступа
 MENUS = {
     'burger': burger_menu,
     'italy': italy_menu,
     'sushi': sushi_menu
 }
+
+__all__ = ['burger_menu', 'italy_menu', 'sushi_menu', 'MENUS']
