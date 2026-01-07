23d2ccee2b020166c98d1bd7ba3a7ecadd012bc7
Muzaffar <160494337+Muzaffar20081@users.noreply.github.com>
2026-01-07 10:12:37 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,2 @@
 # config.py
 BOT_TOKEN = "8244967100:AAF67beMM450dqwz1q0DjnFJohkMl0qjXAE"
-
-# Кухни для меню
-CUISINES = {
-    "burger": "🍔 Бургеры NYC",
-    "italy": "🍕 Итальянская",
-    "sushi": "🍣 Суши"
-}
-
