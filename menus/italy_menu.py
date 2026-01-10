d9b02fc0ab6f43725428f928655e6e3678311978
Muzaffar <160494337+Muzaffar20081@users.noreply.github.com>
2026-01-10 10:49:07 -0500

    Update italy_menu.py

--- menus/italy_menu.py
+++ menus/italy_menu.py
@@ -1,93 +1,67 @@
-# menus/italy_menu.py
+# menus/pizza_menu.py
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-class ItalyMenu:
+class PizzaMenu:
     def __init__(self):
-        self.name = "🍕 Итальянская кухня"
+        self.name = "🍕 Пицца"
         self.icon = "🍕"
-        self.description = "Настоящая итальянская кухня от шеф-повара"
+        self.category = "pizza"
         
         self.items = [
             {
                 "id": "pizza_margherita",
-                "name": "Пицца Маргарита",
-                "description": "Классическая пицца с томатами и моцареллой",
+                "name": "Маргарита",
+                "description": "Классическая пицца с моцареллой и томатами",
                 "price": 550,
-                "size": "30 см",
-                "weight": 480,
-                "calories": 820,
-                "cooking_time": 20
+                "size": "30 см"
             },
             {
                 "id": "pizza_pepperoni",
-                "name": "Пицца Пепперони",
-                "description": "Острая салями, моцарелла, томатный соус",
+                "name": "Пепперони",
+                "description": "Острая салями, сыр, томатный соус",
                 "price": 650,
-                "size": "30 см",
-                "weight": 520,
-                "calories": 890,
-                "cooking_time": 22
+                "size": "30 см"
             },
             {
-                "id": "pasta_carbonara",
-                "name": "Паста Карбонара",
-                "description": "Спагетти, бекон, сливочный соус, пармезан",
-                "price": 480,
-                "weight": 350,
-                "calories": 650,
-                "cooking_time": 15
+                "id": "pizza_4cheese",
+                "name": "4 Сыра",
+                "description": "Смесь четырех видов сыра",
+                "price": 600,
+                "size": "30 см"
             },
             {
-                "id": "tiramisu",
-                "name": "Тирамису",
-                "description": "Классический итальянский десерт",
-                "price": 320,
-                "weight": 180,
-                "calories": 380,
-                "cooking_time": 5
+                "id": "pizza_hawaiian",
+                "name": "Гавайская",
+                "description": "Курица, ананас, сыр, соус",
+                "price": 580,
+                "size": "30 см"
             }
         ]
     
     def get_menu_text(self):
-        """Генерирует текст для меню"""
-        text = f"<b>{self.name}</b>\n"
-        text += f"<i>{self.description}</i>\n\n"
-        text += "<b>Наше меню:</b>\n\n"
-        
-        for idx, item in enumerate(self.items, 1):
-            emoji = "🍕" if "pizza" in item["id"] else "🍝" if "pasta" in item["id"] else "🍰"
-            size_info = f" | 📏 {item['size']}" if 'size' in item else ""
-            
-            text += (
-                f"{idx}. {emoji} <b>{item['name']}</b>\n"
-                f"   💰 {item['price']}₽ | ⚖️ {item['weight']}г{size_info}\n"
-                f"   ⏱️ {item['cooking_time']} мин | {item['description']}\n\n"
-            )
-        
+        text = f"<b>{self.name}</b>\n\n"
+        for item in self.items:
+            text += f"🍕 <b>{item['name']}</b>\n"
+            text += f"   💰 {item['price']}₽ | 📏 {item['size']}\n"
+            text += f"   {item['description']}\n\n"
         return text
     
     def get_keyboard(self):
-        """Создает клавиатуру для меню"""
-        keyboard_buttons = []
-        
+        buttons = []
         for item in self.items:
-            emoji = "🍕" if "pizza" in item["id"] else "🍝" if "pasta" in item["id"] else "🍰"
-            keyboard_buttons.append([
+            buttons.append([
                 InlineKeyboardButton(
-                    text=f"{emoji} {item['name']} - {item['price']}₽",
-                    callback_data=f"menu_item:italy:{item['id']}"
+                    text=f"{item['name']} - {item['price']}₽",
+                    callback_data=f"item:{self.category}:{item['id']}"
                 )
             ])
-        
-        keyboard_buttons.append([
-            InlineKeyboardButton(text="◀️ Назад", callback_data="back_to_main")
+        buttons.append([
+            InlineKeyboardButton(text="◀️ Назад", callback_data="main_menu")
         ])
-        
-        return InlineKeyboardMarkup(inline_keyboard=keyboard_buttons)
+        return InlineKeyboardMarkup(inline_keyboard=buttons)
     
-    def get_item_details(self, item_id):
-        """Получает детальную информацию о блюде"""
+    def get_item(self, item_id):
         for item in self.items:
             if item["id"] == item_id:
                 return item
