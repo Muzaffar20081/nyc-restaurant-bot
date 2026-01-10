dbe8254f53af1f7bd9c33264e99852740ed37cde
Muzaffar <160494337+Muzaffar20081@users.noreply.github.com>
2026-01-10 10:45:05 -0500

    Update burger_menu.py

--- menus/burger_menu.py
+++ menus/burger_menu.py
@@ -4,84 +4,64 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 class BurgerMenu:
     def __init__(self):
-        self.name = "🍔 Бургеры NYC"
+        self.name = "🍔 Бургеры"
         self.icon = "🍔"
-        self.description = "Американские бургеры в нью-йоркском стиле"
+        self.category = "burger"
         
         self.items = [
             {
                 "id": "burger_classic",
-                "name": "Классический NYC бургер",
-                "description": "Говяжья котлета 200г, салат, помидор, лук, соленые огурчики, фирменный соус",
+                "name": "Классический бургер",
+                "description": "Говяжья котлета 200г, салат, помидор, сыр, соус",
                 "price": 350,
-                "weight": 380,
-                "calories": 560,
-                "cooking_time": 15
+                "weight": 380
             },
             {
                 "id": "burger_cheese",
-                "name": "Чизбургер с беконом",
-                "description": "Двойной сыр, бекон, карамелизированный лук, соус BBQ",
-                "price": 490,
-                "weight": 450,
-                "calories": 720,
-                "cooking_time": 18
+                "name": "Двойной чизбургер",
+                "description": "Две котлеты, три слоя сыра, бекон, BBQ соус",
+                "price": 450,
+                "weight": 420
             },
             {
                 "id": "burger_spicy",
                 "name": "Острый бургер",
-                "description": "Котлета из мраморной говядины, халапеньо, острый сыр",
-                "price": 420,
-                "weight": 400,
-                "calories": 580,
-                "cooking_time": 16
+                "description": "Котлета из мраморной говядины, халапеньо, острый соус",
+                "price": 380,
+                "weight": 400
             },
             {
                 "id": "burger_chicken",
                 "name": "Чикенбургер",
-                "description": "Куриная котлета в панировке, салат, помидор, соус тартар",
-                "price": 380,
-                "weight": 360,
-                "calories": 480,
-                "cooking_time": 14
+                "description": "Куриная котлета в панировке, салат, соус тартар",
+                "price": 320,
+                "weight": 350
             }
         ]
     
     def get_menu_text(self):
-        """Генерирует текст для меню"""
-        text = f"<b>{self.name}</b>\n"
-        text += f"<i>{self.description}</i>\n\n"
-        text += "<b>Наше меню:</b>\n\n"
-        
-        for idx, item in enumerate(self.items, 1):
-            text += (
-                f"{idx}. <b>{item['name']}</b>\n"
-                f"   💰 {item['price']}₽ | ⚖️ {item['weight']}г | ⏱️ {item['cooking_time']} мин\n"
-                f"   {item['description']}\n\n"
-            )
-        
+        text = f"<b>{self.name}</b>\n\n"
+        for item in self.items:
+            text += f"🍔 <b>{item['name']}</b>\n"
+            text += f"   💰 {item['price']}₽ | ⚖️ {item['weight']}г\n"
+            text += f"   {item['description']}\n\n"
         return text
     
     def get_keyboard(self):
-        """Создает клавиатуру для меню"""
-        keyboard_buttons = []
-        
+        buttons = []
         for item in self.items:
-            keyboard_buttons.append([
+            buttons.append([
                 InlineKeyboardButton(
                     text=f"{item['name']} - {item['price']}₽",
-                    callback_data=f"menu_item:burger:{item['id']}"
+                    callback_data=f"item:{self.category}:{item['id']}"
                 )
             ])
-        
-        keyboard_buttons.append([
-            InlineKeyboardButton(text="◀️ Назад", callback_data="back_to_main")
+        buttons.append([
+            InlineKeyboardButton(text="◀️ Назад", callback_data="main_menu")
         ])
-        
-        return InlineKeyboardMarkup(inline_keyboard=keyboard_buttons)
+        return InlineKeyboardMarkup(inline_keyboard=buttons)
     
-    def get_item_details(self, item_id):
-        """Получает детальную информацию о блюде"""
+    def get_item(self, item_id):
         for item in self.items:
             if item["id"] == item_id:
                 return item
